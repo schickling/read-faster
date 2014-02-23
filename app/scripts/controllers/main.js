7ecee7edd28407befd5139409692b1e1582d8ac8
Johannes Schickling <schickling.j@gmail.com>
2014-02-23 20:36:53 +0100

    First prototype

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,5 @@
 
 angular.module('readFasterApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.text = "Insert your text here...";
   });
--- app/scripts/directives/ticker.js
+++ app/scripts/directives/ticker.js
@@ -3,11 +3,34 @@
 angular.module('readFasterApp')
   .directive('ticker', function() {
     return {
-      template: '<span>{{word}}</span>',
+      template: '<span></span>',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
-        scope.word = "Test";
-        // element.text('this is the ticker directive');
+
+        var span = element.children()[0],
+          i = 0,
+          words = [];
+
+        parse(scope.text);
+        renderNextWord();
+
+        setInterval(function() {
+          renderNextWord();
+        }, 400);
+
+        scope.$watch('text', function(text) {
+          parse(text);
+          i = 0;
+        });
+
+        function renderNextWord() {
+          span.innerHTML = words[i++ % words.length];
+        }
+
+        function parse(text) {
+          words = scope.text.split(/[\s|\n]/);
+        }
+
       }
     };
   });
